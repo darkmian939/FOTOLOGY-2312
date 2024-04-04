3a4a30e61ea32c88e27d43ca88cc89c907879b92
darkmian939 <cerikdamian@gmail.com>
2024-04-04 00:41:35 -0500

    guardo los modelos dbcontext startup y demas cosas

---
+++ SenaFotology/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+public class FotologyDbContext : DbContext
+{
+    public FotologyDbContext(DbContextOptions<FotologyDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Administrador> Administradores { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Fotografo> Fotografos { get; set; }
+    public DbSet<Portafolio> Portafolios { get; set; }
+    public DbSet<Fotografia> Fotografias { get; set; }
+    public DbSet<Categorias> Categorias { get; set; }
+    public DbSet<Calificacion> Calificaciones { get; set; }
+    public DbSet<Comentario> Comentarios { get; set; }
+}
