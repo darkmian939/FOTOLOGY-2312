3899898834f20b4691083bea10fa47ac7fd06643
Snnat19 <140465833+Snnat19@users.noreply.github.com>
2024-04-04 02:47:03 -0500

    fotografos

--- SenaFotology/DbContext.cs
+++ SenaFotology/DbContext.cs
@@ -1,18 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Data.Entity;
+﻿//using SenaFotology;
+//using System.Data.Entity;
 
-public class FotologyDbContext : DbContext
-{
-    public FotologyDbContext(DbContextOptions<FotologyDbContext> options) : base(options)
-    {
-    }
+//public class FotologyDbContext : DbContext
+//{
+//    public FotologyDbContext(DbContextOptions<FotologyDbContext> options) : base(options)
+//    {
+//    }
 
-    public DbSet<Administrador> Administradores { get; set; }
-    public DbSet<Cliente> Clientes { get; set; }
-    public DbSet<Fotografo> Fotografos { get; set; }
-    public DbSet<Portafolio> Portafolios { get; set; }
-    public DbSet<Fotografia> Fotografias { get; set; }
-    public DbSet<Categorias> Categorias { get; set; }
-    public DbSet<Calificacion> Calificaciones { get; set; }
-    public DbSet<Comentario> Comentarios { get; set; }
-}
+//    public DbSet<Administrador> Administradores { get; set; }
+//    public DbSet<Cliente> Clientes { get; set; }
+//    public DbSet<Fotografo> Fotografos { get; set; }
+//    public DbSet<Portafolio> Portafolios { get; set; }
+//    public DbSet<Fotografia> Fotografias { get; set; }
+//    public DbSet<Categorias> Categorias { get; set; }
+//    public DbSet<Calificacion> Calificaciones { get; set; }
+//    public DbSet<Comentario> Comentarios { get; set; }
+//}
+
+//namespace SenaFotology
+//{
+//    class DbContextOptions<T>
+//    {
+//    }
+//}
