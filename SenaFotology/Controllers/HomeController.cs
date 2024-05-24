456dae7bc261a136a56e0546b16c476bd0b82273
darkmian939 <cerikdamian@gmail.com>
2024-05-23 22:11:16 -0500

    feat(frontend): bases de datos fotografo y administrador

--- SenaFotology/Controllers/HomeController.cs
+++ SenaFotology/Controllers/HomeController.cs
@@ -36,6 +36,21 @@ namespace SenaFotology.Controllers
             return View();
         }
 
+        public IActionResult RegistroUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult RegistroFotografo()
+        {
+            return View();
+        }
+
+        public IActionResult RegistroAdministrador()
+        {
+            return View();
+        }
+
         public IActionResult Ayuda()
         {
             return View();
@@ -51,12 +66,12 @@ namespace SenaFotology.Controllers
 
                 using (var cmd = db.CreateCommand())
                 {
-                    cmd.CommandText = "SELECT email FROM users";
+                    cmd.CommandText = "SELECT Email FROM Cliente";
                     using (var reader = cmd.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            users.Add(reader.GetString("email"));
+                            users.Add(reader.GetString("Email"));
                         }
                     }
                 }
@@ -66,30 +81,156 @@ namespace SenaFotology.Controllers
             return View();
         }
 
+        public IActionResult GetFotografos()
+        {
+            List<string> fotografos = new List<string>();
+
+            using (var db = new DatabaseContext(_connectionString))
+            {
+                db.OpenConnection();
+
+                using (var cmd = db.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Email FROM Fotografo";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            fotografos.Add(reader.GetString("Email"));
+                        }
+                    }
+                }
+            }
+
+            ViewBag.Fotografos = fotografos;
+            return View();
+        }
+
+        public IActionResult GetAdministradores()
+        {
+            List<string> administradores = new List<string>();
+
+            using (var db = new DatabaseContext(_connectionString))
+            {
+                db.OpenConnection();
+
+                using (var cmd = db.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Email FROM Administrador";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            administradores.Add(reader.GetString("Email"));
+                        }
+                    }
+                }
+            }
+
+            ViewBag.Administradores = administradores;
+            return View();
+        }
+
         [HttpPost]
         public IActionResult InicioUsuario(string email, string password)
         {
             if (AuthenticateUser(email, password))
             {
-                // Redirect to the client page or dashboard if login is successful
                 return RedirectToAction("PaginaCliente", "Cliente");
             }
             else
             {
-                // Show an error message if login fails
-                ViewData["Error"] = "Invalid email or password.";
+                ViewData["Error"] = "Invalid Email or password.";
                 return View();
             }
         }
 
-        private bool AuthenticateUser(string email, string password)
+        [HttpPost]
+        public IActionResult InicioFotografo(string email, string password)
+        {
+            if (AuthenticateFotografo(email, password))
+            {
+                return RedirectToAction("PagFotografo", "Fotografo");
+            }
+            else
+            {
+                ViewData["Error"] = "Invalid Username or password.";
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult InicioAdministrador(string email, string password)
+        {
+            if (AuthenticateAdministrador(email, password))
+            {
+                return RedirectToAction("PagAdministrador", "Administrador");
+            }
+            else
+            {
+                ViewData["Error"] = "Invalid Username or password.";
+                return View();
+            }
+        }
+
+private bool AuthenticateUser(string email, string password)
         {
             using (var connection = new MySqlConnection(_connectionString))
             {
                 try
                 {
                     connection.Open();
-                    string query = "SELECT COUNT(*) FROM users WHERE email = @Email AND password = @Password";
+                    string query = "SELECT COUNT(*) FROM Cliente WHERE Email = @Email AND Contrasena = @Password";
+                    using (var command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Email", email);
+                        command.Parameters.AddWithValue("@Password", password);
+
+                        var result = Convert.ToInt32(command.ExecuteScalar());
+                        return result > 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    return false;
+                }
+            }
+        }
+
+        private bool AuthenticateFotografo(string email, string password)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT COUNT(*) FROM Fotografo WHERE Email = @Email AND Contrasena = @Password";
+                    using (var command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Email", email);
+                        command.Parameters.AddWithValue("@Password", password);
+
+                        var result = Convert.ToInt32(command.ExecuteScalar());
+                        return result > 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    return false;
+                }
+            }
+        }
+
+        private bool AuthenticateAdministrador(string email, string password)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT COUNT(*) FROM Administrador WHERE Email = @Email AND Contrasena = @Password";
                     using (var command = new MySqlCommand(query, connection))
                     {
                         command.Parameters.AddWithValue("@Email", email);
